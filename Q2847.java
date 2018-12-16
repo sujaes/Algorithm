1493dd89c331795cef6007e46248feea319483e5
장수제/201311240 <sujaes@naver.com>
2018-12-16 16:37:52 +0900

    update array problems

---
+++ Q2847.java
@@ -0,0 +1,24 @@
+package git;
+
+import java.util.*;
+
+class Q2847{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a= sc.nextInt();
+		int b[] = new int [a];
+		int count = 0 ;
+
+		for( int i = 0 ; i <a; i++){
+			b[i] = sc.nextInt();
+		}
+		for( int j = a-1;j>0;j--){
+			if(b[j]<=b[j-1]){
+				count += b[j-1]-b[j]+1;
+				b[j-1]=b[j]-1;
+			}
+		}
+		System.out.println(count);
+	}
+}
