2c8fb30fd3d5914630292017ed57f90ced744de9
JSJ <sujaes@naver.com>
2020-04-07 22:13:21 +0900

    beakjoon 7568 bruteforce

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -104,3 +104,34 @@
 //		System.out.println(burg + bever -50);
 //	}
 //}
+
+import java.util.*;
+public class Main{
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int person[][] = new int[n][2];
+		int rank[] = new int[n];
+		for(int i = 0 ; i < n; i++){
+			rank[i] = 1;
+			person[i][0] = sc.nextInt();
+			person[i][1] = sc.nextInt();
+		}
+		
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0; j <n ; j++) {
+				if(person[i][0] > person[j][0] && person[i][1] > person[j][1]) {
+					rank[j]++;
+				}
+			}
+		}
+		
+		for(int i=0; i<n;i++) {		
+			System.out.print(rank[i]+" ");
+		}
+	
+	}
+}
+
