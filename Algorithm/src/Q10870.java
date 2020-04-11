0a55cb17286c4b4ac130d2bea76b03b3884b36c8
JSJ <sujaes@naver.com>
2020-04-11 23:23:09 +0900

    beakjoon 10870

---
+++ Algorithm/src/Q10870.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Q10870{
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n+1];
+		arr[0] = 0;
+		arr[1] = 1;
+		for(int i =2;i<=n;i++) {
+			arr[i] = arr[i-2] + arr[i-1];
+		}
+		System.out.println(arr[n]);
+	}
+}
