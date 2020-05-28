5e3b587655c97cefd0bd81b439d79b240687daaf
JSJ <sujaes@naver.com>
2020-05-28 22:09:15 +0900

    Q2178 beakjoon

--- Algorithm/src/Q2178.java
+++ Algorithm/src/Q2178.java
@@ -1,60 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
  
 public class Q2178 {
     static int n;
-    static int m;
     static int[][] arr;
     static boolean[][] visited;
     static int result = 0;
     static Queue<Pair> q;
     static int dx[] = {1,-1,0,0};
     static int dy[] = {0,0,1,-1};
-    static int count = 0;
+    static int count ;
+    static int level = 0;
+    static List<Integer> list;
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         n = sc.nextInt();
-        m = sc.nextInt();
-        
-        arr = new int[n][m];
-        q = new LinkedList<Pair>();
+        arr = new int[n][n];
+        visited = new boolean[n][n];
+        list = new ArrayList<Integer>();
         
         for(int i = 0 ; i < n ; i++) {
-    		String temp = sc.next();
-        	for(int j= 0 ; j < m ; j++) {
+        	String temp  = sc.next();
+        	for(int j = 0 ; j < n ; j++) {
         		arr[i][j] = Integer.parseInt(temp.charAt(j)+"");
         	}
+        }  
+        
+        
+        for(int i = 0 ; i < n; i++) {
+        	for(int j = 0 ; j < n ; j++) {
+        		if(arr[i][j] == 1 && !visited[i][j]) {
+        	        q = new LinkedList<Pair>();
+        			level++;
+        			count = 1;
+        			bfs(new Pair(i,j));
+        		}
+        	}
         }
-        
-        visited = new boolean[n][m];
-        
-        bfs(new Pair(0,0));
+        Collections.sort(list);
       
+        System.out.println(level);
+        for(int i = 0 ; i < list.size();i++) {
+        	System.out.println(list.get(i));
+        }
     }
     
     public static void bfs(Pair p) {
     	q.add(p);
-    	visited[p.a][p.b] = true;
+    	visited[p.a][p.b]=true;
     	while(!q.isEmpty()) {
-    		count++;
     		Pair pp = q.poll();
-    		for(int i =0; i < 4; i++) {
-    			int nx = pp.a + dx[i];
-    			int ny = pp.b + dy[i];
-    			if(nx <0 || nx>= n || ny<0 || ny>=m) {
+    		for(int i = 0 ; i<4; i++) {
+    			int nx = pp.a+ dx[i];
+    			int ny = pp.b+ dy[i];
+    			
+    			if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
     				continue;
     			}
     			if(arr[nx][ny]==1 && !visited[nx][ny]) {
+    				visited[nx][ny] =true;
+    				count++;
     				q.add(new Pair(nx,ny));
-    				visited[nx][ny] = true;
-    				arr[nx][ny] = arr[pp.a][pp.b]+1;
     			}
+    			
     		}
     	}
-    	System.out.println(arr[n-1][m-1]);
+    	list.add(count);
     }
     
     
