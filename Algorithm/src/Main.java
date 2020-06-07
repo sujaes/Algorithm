d5fa57aa8cea3fe9fe0e1cb7409bb9dd302deeb8
JSJ <sujaes@naver.com>
2020-06-07 22:58:01 +0900

    c

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,26 +1,25 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
- 
+
 public class Main {
-    static int n;    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        n = sc.nextInt();
-        int d[][] = new int[n+1][3];
-        d[1][0] = 1;
-        d[1][1] = 1;
-        d[1][2] = 1;
-        
-        for(int i = 2 ; i <= n ;i++){
-            d[i][0]=(d[i-1][1]+d[i-1][2])%9901;
-            d[i][1]=(d[i-1][0]+d[i-1][2])%9901;
-            d[i][2]=(d[i-1][1]+d[i-1][2]+d[i-1][0])%9901;
+        char[][] input = new char[5][15];
+        int max_length = Integer.MIN_VALUE;
+    	
+        for(int i = 0; i < 5; i++) {
+            String s = sc.nextLine().trim();
+            max_length = Math.max(max_length, s.length());
+            for(int j = 0; j < s.length(); j++) {
+                input[i][j] = s.charAt(j);
+            }
         }
-        
-        System.out.println((d[n][0]+d[n][1]+d[n][2])%9901);
-    
+        for(int i = 0; i < max_length; i++) {
+            for(int j = 0; j < 5; j++) {
+                if(input[j][i] == '\0')
+                    continue;
+                System.out.print(input[j][i]);
+            }
+        }
     }
-    
 }
