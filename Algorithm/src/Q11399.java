b168d32fb4d8b9999b41c2a30ba8f24e5068c4e5
JSJ <sujaes@naver.com>
2020-10-21 23:30:23 +0900

    백준 11399

---
+++ Algorithm/src/Q11399.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Q11399 {
+	static int n,m;
+	static boolean visited[];
+	static int arr[];
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	arr = new int[n];
+    	int sum =0 ;
+    	for(int i = 0 ; i < n ; i++) {
+    		arr[i] = sc.nextInt();
+    	}
+    	Arrays.sort(arr);
+    	
+    	int temp =0;
+    	for(int i = 0 ; i <n ;i++) {
+    		temp += arr[i];
+    		sum += temp;
+    	}
+    	System.out.println(sum);
+    	
+    }
+}
