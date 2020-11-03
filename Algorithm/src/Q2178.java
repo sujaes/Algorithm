b66b87a1509b85ff1fff6ac7cc6c880b7a14fc3a
jsj <sujaes@naver.com>
2020-11-03 14:49:03 +0900

    백준 2178 미로탐색 bfs

--- Algorithm/src/Q2178.java
+++ Algorithm/src/Q2178.java
@@ -84,3 +84,62 @@ public class Q2178 {
     	}
     }
 }
+
+//2020.11.03
+//import java.util.*;
+//
+//public class Main {
+//	static int n,m;
+//	static int arr[][];
+//	static boolean visited[][];
+//	static Queue<Node> q;
+//	static int dx[] = {0,0,1,-1};
+//	static int dy[] = {1,-1,0,0};
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		n = sc.nextInt();
+//		m = sc.nextInt();
+//		arr = new int[n][m];
+//		visited = new boolean[n][m];
+//		q = new LinkedList<Node>();
+//		
+//		
+//		for(int i = 0; i <n ; i++) {
+//			String s = sc.next();
+//			for(int j = 0 ; j < m ; j++) {
+//				arr[i][j] = Integer.parseInt(s.charAt(j)+"");
+//			}
+//		}
+//		q.add(new Node(0,0));
+//		visited[0][0] = true;
+//		bfs();
+//		System.out.println(arr[n-1][m-1]);
+//		
+//	}
+//	public static void bfs() {
+//		while(!q.isEmpty()) {
+//			Node node = q.poll();
+//			for(int i = 0 ; i < 4; i++) {
+//				int nx = node.x + dx[i];
+//				int ny = node.y + dy[i];
+//				if(nx < 0 || ny <0 || nx>=n || ny>=m) {
+//					continue;
+//				}
+//				if(arr[nx][ny]!=0 && !visited[nx][ny]) {
+//					q.add(new Node(nx,ny));
+//					arr[nx][ny] = arr[node.x][node.y] +1;
+//					visited[nx][ny] = true;
+//				}
+//			}
+//		}
+//	}
+//}
+//class Node{
+//	int x;
+//	int y;
+//	public Node(int x, int y) {
+//		this.x = x;
+//		this.y = y;
+//	}
+//	
+//}
