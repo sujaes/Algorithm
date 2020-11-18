21b0e3fecc3b4166d6357584481d4b0bd6b71894
jangsujae <sujaes@naver.com>
2020-11-18 21:28:03 +0900

    백준 9095 123더하기 dp

---
+++ Algorithm/src/Q9095.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+public class Q9095 {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        int n;
+        int[] dp = new int[11]; 
+        dp[1] = 1; 
+        dp[2] = 2; 
+        dp[3] = 4; 
+        for(int i=0; i<t; i++){
+            n = scanner.nextInt();      
+            for(int j=4; j<=n; j++){
+                dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+            }
+            System.out.println(dp[n]);
+        }
+    }
+}
