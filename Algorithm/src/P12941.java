eb53ccfca401d4959561547a46c1e7b95dc71f14
jangsujae <sujaes@naver.com>
2021-01-12 16:15:59 +0900

    프로그래머스 level2 최솟값만들기 수정

--- Algorithm/src/P12941.java
+++ Algorithm/src/P12941.java
@@ -44,3 +44,24 @@
 //		 }
 //		 
 //}
+
+
+//import java.util.Arrays;
+//class Solution
+//{
+//    public int solution(int []A, int []B)
+//    {
+//        int answer = 0;	
+//        int j = B.length - 1;
+//        
+//        Arrays.sort(A);
+//        Arrays.sort(B);
+//        
+//        for(int i = 0; i<A.length; i++) {
+//        	answer = answer + (A[i] * B[j]);
+//        	j--;
+//        }
+//
+//        return answer;
+//    }
+//}
