878b1e8990ec48eccb38b14281d8c0b9cab62b56
jangsujae <sujaes@naver.com>
2021-01-13 19:13:51 +0900

    프로그래머스 level2 숫자의표현

---
+++ Algorithm/src/P12924.java
@@ -0,0 +1,16 @@
+//class Solution {
+//    public int solution(int n) {
+//        int answer = 1;
+//        for(int i = 1; i<= n/2; i++ ){
+//            int sum = 0;
+//            int d = i;
+//            while(sum<n){
+//                sum+=d++;
+//            }
+//            if(sum==n){
+//                answer++;
+//            }
+//        }
+//        return answer;
+//    }
+//}
