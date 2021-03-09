b6caec47f317454b3e424ab28eb0ebe6f781b1e2
jsj <sujaes@naver.com>
2021-03-09 12:42:15 +0900

    코딜리티 lesson3_1 FrogJmp

---
+++ Algorithm/src/Clesson3_1.java
@@ -0,0 +1,13 @@
+//class Solution {
+//    public int solution(int X, int Y, int D) {
+//        // write your code in Java SE 8
+//        int dis = Y-X;
+//        int answer = 0;
+//        if(dis%D == 0){
+//            answer = dis/D;
+//        }else{
+//            answer = dis/D + 1;
+//        }
+//        return answer;
+//    }
+//}
