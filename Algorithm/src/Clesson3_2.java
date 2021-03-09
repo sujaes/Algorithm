47218486b0c311dbfd729b7511838585fe8824d6
jsj <sujaes@naver.com>
2021-03-09 12:42:39 +0900

    코딜리티 lesson3_2 PermMissingElem

---
+++ Algorithm/src/Clesson3_2.java
@@ -0,0 +1,17 @@
+//import java.util.*;
+//class Solution {
+//    public int solution(int[] A) {
+//        HashMap<Integer, String> map = new HashMap<>(); 
+//        int answer =0;
+//        for (int i=1; i<=A.length+1; i++){
+//            map.put(i, "pass");
+//        }
+//        for (int i : A){
+//            map.remove(i);
+//        }
+//        for (int key : map.keySet()) {
+//            answer = key;
+//        }
+//        return answer;
+//    }
+//}
