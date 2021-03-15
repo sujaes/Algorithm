72d117c7558d69aedbd6927821c25fef97dd4876
jangsujae <sujaes@naver.com>
2021-03-15 20:40:43 +0900

    코딜리티 lesson4_1 FrogRiverOne

---
+++ Algorithm/src/Clesson4_1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int X, int[] A) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int leng = A.length;
+        int count = 0;
+        boolean flag = true;
+        for(int i = 1; i <=X; i++){
+            map.put(i,1);
+        }
+
+        for(int i = 0 ; i < leng;i++){
+            if(map.containsKey(A[i])){
+                map.remove(A[i]);
+            }
+            if(map.size() == 0){
+                flag = false;
+                break;
+            }
+            count++;
+        }
+        if(flag){
+            return -1;
+        }else{
+            return count;
+        }
+    }
+}
