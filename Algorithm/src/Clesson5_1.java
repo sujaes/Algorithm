c966c8e5a14c153ae2879fe3a2714d405971d072
jsj <sujaes@naver.com>
2021-03-19 11:31:01 +0900

    코딜리티 lesson5_1 CountDiv

---
+++ Algorithm/src/Clesson5_1.java
@@ -0,0 +1,23 @@
+//public class Solution {
+//    public int solution(int A, int B, int K) {
+//        int init = 0;
+//        for (int i=A; i<=B; i++) {
+//            if (i % K == 0) {
+//                init = i;
+//                break;
+//            }
+//        }
+//
+//        if (A==B){
+//            if(A%K ==0){
+//                return 1;
+//            }else{
+//                return 0;
+//            }
+//        }
+//        
+//        int result = B/K - init/K + 1;
+//        
+//        return result;
+//    }
+// }
