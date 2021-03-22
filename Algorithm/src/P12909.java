d5910ac9aa39d1d14feee95dcf7246a898b7591f
jangsujae <sujaes@naver.com>
2021-03-22 14:11:45 +0900

    프로그래머스 level2 올바른괄호

--- Algorithm/src/P12909.java
+++ Algorithm/src/P12909.java
@@ -1,4 +1,40 @@
-
-public class P12909 {
-
-}
+//import java.util.*;
+//class Solution {
+//    boolean solution(String s) {
+//        // Stack<Character> stack = new Stack<Character>();
+//        int count= 0;
+//        int len = s.length();
+//        if(len%2 !=0 ){
+//            return false;
+//        }
+//        if(s.charAt(0)==')'){
+//            return false;
+//        }
+//        if(s.charAt(len-1)=='('){
+//            return false;
+//        }
+//        
+//        
+//        for(int i = 0 ; i <len;i++){
+//            if(s.charAt(i)=='('){
+//                count++;
+//                // stack.add('(');
+//            }else{
+//                // if(stack.isEmpty()){
+//                //     return false;
+//                // }
+//                if(count == 0){
+//                    return false;
+//                }
+//                count--;
+//                // stack.pop();
+//            }
+//        }
+//        if(count==0){
+//            return true;
+//        }else{
+//            return false;
+//        }
+//
+//    }
+//}
