0cf327752ebd39cd040707ee950692a46a33d192
jangsujae <sujaes@naver.com>
2021-04-05 20:34:54 +0900

    프로그래머스 level3 숫자게임

---
+++ Algorithm/src/P12987.java
@@ -0,0 +1,22 @@
+//import java.util.*;
+//class Solution {
+//    public int solution(int[] A, int[] B) {
+//        int answer = 0;
+//        Arrays.sort(A); //1357
+//        Arrays.sort(B); //2268
+//        int a = 0;  //a인덱스
+//        int b = 0;  //b인덱스
+//        for (int i = 0; i < A.length; i++) {
+//            if (A[a] > B[b]) {      //a가 더크면 b인덱스 증가
+//                b++;
+//            } else if (A[a] == B[b]) {  //같으면 a인덱스 증가
+//                a++;
+//            } else {                //b가 더 크면 둘다증가하고 점수 올리기
+//                a++;
+//                b++;
+//                answer++;
+//            }
+//        }
+//        return answer;
+//    }
+//}
