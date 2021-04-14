8d857fbb04bd433ae23d993e049635a7e769af0f
jangsujae <sujaes@naver.com>
2021-04-15 00:55:06 +0900

    백준 14889 스타트와링크(삼성 SW역량 테스트)

--- Algorithm/src/Q14889.java
+++ Algorithm/src/Q14889.java
@@ -1,52 +1,104 @@
+//import java.util.Scanner;
+//
+//class Q14889 {
+//	static int n;
+//	static int arr[][];
+//	static int min = Integer.MAX_VALUE;
+//	static boolean visited[];
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        arr = new int[n+1][n+1];
+//        visited = new boolean[n+1];
+//        
+//        for(int i = 1; i <=n ;i++) {
+//        	for(int j =1; j<=n ;j++) {
+//        		arr[i][j] = sc.nextInt();
+//        	}
+//        }
+//        
+//        comb(1,0);
+//        System.out.println(min);
+//    }
+//    public static void comb(int start,int depth) {
+//    	if(depth == n/2) {
+//    		min = Math.min(min, getAbilityDifference());
+//    		return;
+//    	}
+//    	for(int i =start; i<=n; i++) {
+//    		if(!visited[i]) {
+//    			visited[i] = true;
+//    			comb(i+1 , depth+1);
+//    			visited[i] = false;
+//    		}
+//    	}
+//    }
+//    public static int getAbilityDifference() {
+//    	int start= 0;
+//    	int link = 0;
+//    	
+//    	for(int i = 1; i <= n; i++) {
+//    		for(int j = 1; j<=n ;j++) {
+//    			if(visited[i] && visited[j]) {
+//    				start += arr[i][j];
+//    			}else if(!visited[i] && !visited[j]){
+//    				link += arr[i][j];
+//    			}
+//    		}
+//    	}
+//    	return Math.abs(start - link);
+//    }
+//
+//}
+
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Q14889 {
-	static int n;
+	static int n,min=Integer.MAX_VALUE;
 	static int arr[][];
-	static int min = Integer.MAX_VALUE;
 	static boolean visited[];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new int[n+1][n+1];
-        visited = new boolean[n+1];
-        
-        for(int i = 1; i <=n ;i++) {
-        	for(int j =1; j<=n ;j++) {
+        arr = new int[n][n];
+        for(int i = 0 ; i < n ;i++) {
+        	for(int j = 0 ; j < n ;j++) {
         		arr[i][j] = sc.nextInt();
         	}
         }
-        
+        int a[] = new int[n/2];
         comb(1,0);
         System.out.println(min);
     }
     public static void comb(int start,int depth) {
-    	if(depth == n/2) {
-    		min = Math.min(min, getAbilityDifference());
-    		return;
-    	}
-    	for(int i =start; i<=n; i++) {
-    		if(!visited[i]) {
-    			visited[i] = true;
-    			comb(i+1 , depth+1);
-    			visited[i] = false;
-    		}
-    	}
+		if(depth == n/2) {
+			min = Math.min(min, diff());
+			return;
+		}
+		for(int i =start; i<=n; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				comb(i+1 , depth+1);
+				visited[i] = false;
+			}
+		}
     }
-    public static int getAbilityDifference() {
+
+    public static int diff() {
     	int start= 0;
     	int link = 0;
-    	
-    	for(int i = 1; i <= n; i++) {
-    		for(int j = 1; j<=n ;j++) {
-    			if(visited[i] && visited[j]) {
-    				start += arr[i][j];
-    			}else if(!visited[i] && !visited[j]){
-    				link += arr[i][j];
-    			}
-    		}
-    	}
-    	return Math.abs(start - link);
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j<=n ;j++) {
+				if(visited[i] && visited[j]) {
+					start += arr[i][j];
+				}else if(!visited[i] && !visited[j]){
+					link += arr[i][j];
+				}
+			}
+		}
+		return Math.abs(start - link);
     }
 
 }
