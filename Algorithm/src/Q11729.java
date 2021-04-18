f80bb24451ad95f2f6f80d330b239daacdd7b33b
jangsujae <sujaes@naver.com>
2021-04-18 22:14:48 +0900

    백준 11729 하노이탑

---
+++ Algorithm/src/Q11729.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+ 
+public class Q11729 {
+ 
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+ 
+		sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+ 
+		Hanoi(N, 1, 2, 3);
+		System.out.println(sb);
+ 
+	}
+ 
+	/*
+		N : 원판의 개수 
+		start : 출발지 
+		mid : 옮기기 위해 이동해야 장소 
+		to : 목적지
+	 */
+ 
+	public static void Hanoi(int N, int start, int mid, int to) {
+		// 이동할 원반의 수가 1개라면?
+		if (N == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
+		// STEP 1 : N-1개를 A에서 B로 이동
+		Hanoi(N - 1, start, to, mid);
+		
+		// STEP 2 : 1개를 A에서 C로 이동
+		sb.append(start + " " + to + "\n");
+		
+		// STEP 3 : N-1개를 B에서 C로 이동
+		Hanoi(N - 1, mid, start, to);
+ 
+	}
+}
