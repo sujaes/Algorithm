e423a77bc6dd7c3ede40d34bf5ec5ef33507e72c
jsj <sujaes@naver.com>
2021-04-27 14:24:20 +0900

    프로그래머스 level2 더맵게

--- Algorithm/src/P42626.java
+++ Algorithm/src/P42626.java
@@ -56,3 +56,35 @@
 //        return answer;
 //    }
 //}
+
+
+
+
+
+//import java.util.*;
+//
+//class Solution {
+//    public int solution(int[] scoville, int K) {
+//        int answer = 0;
+//        PriorityQueue<Integer> pq = new PriorityQueue();
+//
+//        for (int i = 0 ; i < scoville.length; i++) {
+//            pq.offer(scoville[i]);
+//        }
+//
+//        while (pq.peek() <= K) {
+//            if (pq.size() == 1) {
+//                return -1;
+//            }
+//            int a = pq.poll();
+//            int b = pq.poll();
+//
+//
+//            int result = a + (b * 2);
+//
+//            pq.offer(result);
+//            answer ++;
+//        }
+//        return answer;
+//    }
+//}
