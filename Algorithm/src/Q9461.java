312cad70240fb5df8f4ade67ff3835982ae86b9c
jangsujae <sujaes@naver.com>
2021-05-01 13:52:58 +0900

    백준 9461 파도반수열

--- Algorithm/src/Q9461.java
+++ Algorithm/src/Q9461.java
@@ -1,24 +1,22 @@
-
-//9461¹ø ¹®Á¦
 import java.util.*;
-public class Q9461{
-  static public void main(String args[])
-  {
-      Scanner sc = new Scanner(System.in);
-      int T = sc.nextInt();
-      long dp[] = new long[101];
-      dp[1] = 1; dp[2] = 1; dp[3]= 1; dp[4] = 2;
-      int k=0;
-
-      for(int i =1; i<= T; i++)
-      {
-          k = sc.nextInt();
-          if(k>4)
-          {
-              for(int j =4; j<=k; j++)
-                  dp[j]= dp[j-3]+dp[j-2];
-          }
-          System.out.println(dp[k]);
-      }
-  }
+public class Q9461 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        long dp[] = new long[101];
+        dp[1] = 1; dp[2] = 1; dp[3]= 1; dp[4] = 2; dp[5]=2;
+        int k=0;
+ 
+        for(int i =1; i<= T; i++)
+        {
+            k = sc.nextInt();
+            if(k>5)
+            {
+                for(int j =6; j<=k; j++)
+                    dp[j]= dp[j-1]+dp[j-5];
+            }
+            System.out.println(dp[k]);
+        }
+    }
 }
