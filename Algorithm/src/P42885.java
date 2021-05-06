051a8b3474653789b6e763c6b8b49d308838331d
jsj <sujaes@naver.com>
2021-05-06 16:27:38 +0900

    프로그래머스 level2 구명보트

--- Algorithm/src/P42885.java
+++ Algorithm/src/P42885.java
@@ -11,3 +11,29 @@
 //        return answer;
 //    }
 //}
+
+
+
+//import java.util.*;
+//class Solution {
+//    public int solution(int[] people, int limit) {
+//        Arrays.sort(people);
+//        int answer = 0;
+//        int left = 0;
+//        int right = people.length-1;
+//        int sum = 0;
+//        while(left <= right){
+//            sum = people[left] + people[right];
+//            if(sum <= limit){
+//                left++;
+//                right--;
+//            }else {
+//                right--;
+//            }
+//            answer++;
+//        }
+//        return answer;
+//    }
+//}
+
+
