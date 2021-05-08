ec70abcf78308aec6768236feb79f1f3f80def7c
jangsujae <sujaes@naver.com>
2021-05-09 01:08:42 +0900

    백준 14889 스타트와링크

--- Algorithm/src/Q14889.java
+++ Algorithm/src/Q14889.java
@@ -52,9 +52,7 @@
 //}
 
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
 class Q14889 {
 	static int n,min=Integer.MAX_VALUE;
 	static int arr[][];
@@ -77,7 +75,7 @@ class Q14889 {
 			min = Math.min(min, diff());
 			return;
 		}
-		for(int i =start; i<=n; i++) {
+		for(int i =start; i<=n; i++) {		//1부터 시작해서 n까지 n/2개 고르기
 			if(!visited[i]) {
 				visited[i] = true;
 				comb(i+1 , depth+1);
@@ -86,19 +84,19 @@ class Q14889 {
 		}
     }
 
-    public static int diff() {
+    public static int diff() {				//n/2개 고를때마다 차이 확인
     	int start= 0;
     	int link = 0;
 		for(int i = 1; i <= n; i++) {
 			for(int j = 1; j<=n ;j++) {
-				if(visited[i] && visited[j]) {
+				if(visited[i] && visited[j]) {	//둘다 고른숫자면 start팀에 포함
 					start += arr[i][j];
-				}else if(!visited[i] && !visited[j]){
+				}else if(!visited[i] && !visited[j]){	//둘다 안고른 숫자면 link팀에 포함
 					link += arr[i][j];
 				}
 			}
 		}
-		return Math.abs(start - link);
+		return Math.abs(start - link);			//두수의 차이를 min과 비교해야함
     }
 
 }
