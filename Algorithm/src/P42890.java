0960eded593e9f8bb89c012dd2718e11db22899d
jangsujae <sujaes@naver.com>
2021-05-09 22:48:41 +0900

    프로그래머스 level2 후보키

--- Algorithm/src/P42890.java
+++ Algorithm/src/P42890.java
@@ -1,15 +1,15 @@
 //import java.util.*;
 //
-//public class 후보키 {
-//	String[][] relation;
-//	int colSize;
-//	boolean[] select;
+//public class Solution {
+//	static int colSize;
+//	static boolean[] select;
 //	List<int[]> candidates = new ArrayList<>();
-//
+//	String[][] relation;
+//	
 //	public int solution(String[][] relation) {
 //		this.relation = relation;
-//		this.colSize = relation[0].length;
-//		this.select = new boolean[colSize];
+//		colSize = relation[0].length;			//속성갯수
+//		select = new boolean[colSize];
 //		for (int i = 0; i < colSize; i++) {
 //			dfs(0, 0, i + 1);
 //		}
@@ -20,15 +20,18 @@
 //		if (cnt == selectSize) {
 //			List<Integer> tempColumns = new ArrayList<>();
 //			for (int i = 0; i < colSize; i++) {
-//				if (select[i])
+//				if (select[i]) {
 //					tempColumns.add(i);
+//				}
+//					
 //			}
-//			if (!isUnique(tempColumns))
+//			if (!isUnique(tempColumns)) {
 //				return;
+//			}
 //			int[] candidate = new int[colSize + 1];
 //			for (Integer col : tempColumns) {
-//				candidate[col] = 1;
-//				candidate[colSize]++;
+//				candidate[col] = 1;		//해당속성
+//				candidate[colSize]++;	//최소성확인
 //			}
 //			add(candidate);
 //		}
@@ -48,7 +51,7 @@
 //			for (Integer col : columns) {
 //				sb.append(tuple[col]);
 //			}
-//			if (!tempTuples.add(sb.toString()))
+//			if (!tempTuples.add(sb.toString()))			//이미 있으면 false
 //				return false;
 //		}
 //		return true;
@@ -65,5 +68,55 @@
 //			}
 //		}
 //		this.candidates.add(requestCandidate);
+//	}
+//}
+
+
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//
+//class Solution {
+//	public int solution(String[][] relation) {
+//		int rowSize = relation.length;
+//		int colSize = relation[0].length;
+//		ArrayList<Integer> list = new ArrayList<>();
+//		for (int i = 1; i < (1 << colSize); i++) {
+//			if (!checkMinimality(i, list))
+//				continue;
+//			if (!checkUniqueness(i, relation, rowSize, colSize))
+//				continue;
+//			list.add(i);
+//		}
+//		return list.size();
+//	}
+//
+//	private boolean checkMinimality(int set, ArrayList<Integer> list) {
+//		for (int key : list) {
+//			if ((set & key) == key)
+//				return false;
+//		}
+//		return true;
+//	}
+//
+//	private boolean checkUniqueness(int set, String[][] relation, int rowSize, int colSize) {
+//		ArrayList<Integer> s = getSet(set, colSize);
+//		HashSet<String> hashSet = new HashSet<>();
+//		for (int i = 0; i < rowSize; i++) {
+//			StringBuilder sb = new StringBuilder();
+//			for (int j : s) {
+//				sb.append(relation[i][j]).append(" ");
+//			}
+//			hashSet.add(sb.toString());
+//		}
+//		return hashSet.size() == rowSize;
+//	}
+//
+//	private ArrayList<Integer> getSet(int set, int colSize) {
+//		ArrayList<Integer> result = new ArrayList<>();
+//		for (int i = 0; i < colSize; i++) {
+//			if (((set >> i) & 1) == 1)
+//				result.add(i);
+//		}
+//		return result;
 //	}
 //}
