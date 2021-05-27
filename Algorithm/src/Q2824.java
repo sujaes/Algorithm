bce0c2de94ba704128a659ef80545d69405af641
jangsujae <sujaes@naver.com>
2021-05-28 00:03:51 +0900

    백준 2824 최대공약수

--- Algorithm/src/Q2824.java
+++ Algorithm/src/Q2824.java
@@ -1,51 +1,31 @@
-import java.io.*;
+import java.awt.*;
+import java.math.BigInteger;
 import java.util.*;
+import java.util.List;
 
+//This program prints Welcome to JAVA!
 public class Q2824 {
-	static final int RANGE = 1000000000;
-	static int[] aa;
-	static int[] ba;
-	static int N,M;
-	static long ans = 1;
-	public static void main(String[] args) throws Exception {
-		StringBuilder sb= new StringBuilder();
-		StringTokenizer st = null;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		aa = new int[N];
-		st = new StringTokenizer(br.readLine());
-		boolean flg = false;
-		for(int i=0; i<N; i++) {
-			aa[i]= Integer.parseInt(st.nextToken());
-		}
-		M = Integer.parseInt(br.readLine());
-		ba = new int[M];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			ba[i]=Integer.parseInt(st.nextToken());
-		}
-		for(int i=0; i<N; i++) {
-			if(aa[i]==1) continue;
-			for(int j=0; j<M; j++) {
-				if(ba[j]==1) continue;
-				int num = gcd(aa[i],ba[j]);
-				ans *= num;
-				if(ans > RANGE) flg = true;
-				ans %= RANGE;
-				aa[i] /= num; 
-				ba[j] /= num;
-			}
-		}
-		String s = String.valueOf(ans);
-		int len = s.length();
-		if(len < 9 && flg) {
-			for(int i=0; i<9-len; i++) sb.append("0");
-		}
-		sb.append(ans);
-		System.out.println(sb);
-	}
-	static int gcd(int a, int b) {
-		if(b==0) return a;
-		return gcd(b, a%b);
-	}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n,m;
+        n = sc.nextInt();
+
+        BigInteger a = BigInteger.valueOf(1);
+        for(int i=0; i<n; i++) {
+            a = a.multiply(new BigInteger(sc.next()));
+        }
+        m = sc.nextInt();
+        BigInteger b = BigInteger.valueOf(1);
+        for(int i=0; i<m; i++) {
+            b = b.multiply(new BigInteger(sc.next()));
+        }
+
+        String ans = a.gcd(b).toString();
+        if(ans.length() > 9) {
+            System.out.println(ans.substring(ans.length()-9, ans.length()));
+        } else System.out.println(ans);
+    }
 }
